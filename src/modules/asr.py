48a29b14c9cbe0d1b44f0b7582126ae8e74a1b62
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-03-17 14:31:06 +0100

    feat: translation done by whisper

--- src/modules/asr.py
+++ src/modules/asr.py
@@ -14,58 +14,16 @@ REQUEST_TIMEOUT = int(getenv('REQUEST_TIMEOUT'))
 SAMPLE_JP_FILEPATH = Path(__file__).resolve(
 ).parent.parent / r'audio\samples\japanese_speech_sample.wav'
 SAMPLE_EN_FILEPATH = Path(__file__).resolve(
-).parent.parent / r'audio\samples\english_speech_sample.wav'
+).parent.parent / r'audio/samples/english_speech_sample.wav'
 
-if WHISPER_LOCAL:
-    print(f"[WHISPER] loading up {WHISPER_LOCAL_MODEL} whisper model..")
-    whisper_model = whisper.load_model(WHISPER_LOCAL_MODEL)
-    whisper_model.transcribe(str(SAMPLE_EN_FILEPATH))
-    print(f"[WHISPER] successfully loaded! running on {whisper_model.device}")
-
-
-def transcribe(filepath):
-    if WHISPER_LOCAL:
-        return whisper_model.transcribe(filepath)["text"]
-    try:
-        with open(filepath, 'rb') as infile:
-            files = {'audio_file': infile}
-            r = requests.post(
-                f'{BASE_URL}/asr?task=transcribe&language=en&output=json', files=files)
-
-    except requests.exceptions.Timeout:
-        print('Request timeout')
-        return None
-
-    except requests.exceptions.ConnectionError:
-        print('Unable to reach Whisper, ensure that it is running, or the WHISPER_BASE_URL variable is set correctly')
-        return None
-
-    return r.json()['text'].strip()
+print(f"[WHISPER] loading up {WHISPER_LOCAL_MODEL} whisper model..")
+whisper_model = whisper.load_model(WHISPER_LOCAL_MODEL)
+whisper_model.transcribe(str(SAMPLE_EN_FILEPATH))
+print(f"[WHISPER] successfully loaded! running on {whisper_model.device}")
 
 
-def translate(filepath, language):
-    try:
-        with open(filepath, 'rb') as infile:
-            files = {'audio_file': infile}
-            r = requests.post(f'{BASE_URL}/asr?task=translate&language={language}&output=json',
-                              files=files,
-                              timeout=REQUEST_TIMEOUT)
-    except requests.exceptions.Timeout:
-        print('Request timeout')
-        return None
-
-    except requests.exceptions.ConnectionError:
-        print('Unable to reach Whisper, ensure that it is running, or the WHISPER_BASE_URL variable is set correctly')
-        return None
-
-    translation = r.json()['text'].strip()
-
-    # remove most hallucinations
-    if translation.lower().startswith('thank you for'):
-        return None
-
-    return translation
-
+def transcribe(filepath, language):
+    return whisper_model.transcribe(filepath, language=language)["text"]
 
 if __name__ == '__main__':
     # test if whisper is up and running
@@ -73,7 +31,3 @@ if __name__ == '__main__':
     print(f'Actual audio: Oh. Honestly, I could not be bothered to play this game to full completion.'
           f'The narrator is obnoxious and unfunny, with his humor and dialogue proving to be more irritating than '
           f'entertaining.\nWhisper audio: {transcribe(SAMPLE_EN_FILEPATH)}\n')
-
-    print('Testing Whisper on Japanese speech sample.')
-    print(f'Actual translation: How is this dress? It suits you very well.\n'
-          f"Whisper translation: {translate(SAMPLE_JP_FILEPATH, 'ja')}")
--- src/voice_translator.py
+++ src/voice_translator.py
@@ -1,12 +1,10 @@
 import wave
 from os import getenv
 from time import sleep
+from typing import Optional
 
-import deepl
-import googletrans
-import keyboard
+from pynput import keyboard
 import pyaudio
-import requests
 import time
 
 # LOGGING STUFF
@@ -32,39 +30,27 @@ logger.info("loading")
 from dotenv import load_dotenv
 
 from modules.asr import transcribe
-from modules.tts import speak
+# from modules.tts import speak
 
 load_dotenv()
 
-USE_DEEPL = getenv('USE_DEEPL', 'False').lower() in ('true', '1', 't')
-if not USE_DEEPL:
-    import argostranslate.package
-    import argostranslate.translate
 
-    from_code = "en"
-    to_code = "ja"
-
-    # Download and install Argos Translate package
-    argostranslate.package.update_package_index()
-    available_packages = argostranslate.package.get_available_packages()
-    package_to_install = next(
-        filter(
-            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
-        )
-    )
-    argostranslate.package.install_from_path(package_to_install.download())
-
-DEEPL_AUTH_KEY = getenv('DEEPL_AUTH_KEY')
-TARGET_LANGUAGE = getenv('TARGET_LANGUAGE_CODE')
+TARGET_LANGUAGE = getenv('TARGET_LANGUAGE')
 MIC_ID = int(getenv('MICROPHONE_ID'))
 RECORD_KEY = getenv('MIC_RECORD_KEY')
 LOGGING = getenv('LOGGING', 'False').lower() in ('true', '1', 't')
 MIC_AUDIO_PATH = r'audio/mic.wav'
-CHUNK = 1024
+CHUNK = 4096
 FORMAT = pyaudio.paInt16
 
 
-def on_press_key(_):
+def on_press_key(key):
+    try:
+        if not key.char == RECORD_KEY:
+            return
+    except AttributeError:
+        logger.error(f"special key pressed: {key}")
+        return
     global frames, recording, stream
     if not recording:
         logger.debug("starting recording..")
@@ -78,13 +64,20 @@ def on_press_key(_):
                         input_device_index=MIC_ID)
 
 
-def on_release_key(_):
+def on_release_key(key):
+    try:
+        if not key.char == RECORD_KEY:
+            return
+    except AttributeError:
+        logger.error(f"special key pressed: {key}")
+        return
     logger.debug("ending recording")
     start = time.time()
     global recording, stream
     recording = False
-    stream.stop_stream()
-    stream.close()
+    if stream is not None:
+        stream.stop_stream()
+        stream.close()
     stream = None
 
     # if empty audio file
@@ -102,27 +95,11 @@ def on_release_key(_):
     logger.debug(f"wrote audio file to os | took {time.time() - start}")
 
     # transcribe audio
-    try:
-        eng_speech = transcribe(MIC_AUDIO_PATH)
-        logger.debug(f"[DEBUG] transcribed | took {time.time() - start}")
-    except requests.exceptions.JSONDecodeError:
-        logger.error('Too many requests to process at once')
-        return
+    jp_speech = transcribe(MIC_AUDIO_PATH, TARGET_LANGUAGE)
 
-    if eng_speech:
-        logger.info(f'English: {eng_speech}')
-
-        if USE_DEEPL:
-            jp_speech = translator.translate_text(
-                eng_speech, target_lang=TARGET_LANGUAGE)
-        else:
-            # jp_speech = translator.translate(
-            #     eng_speech, dest=TARGET_LANGUAGE).text
-            jp_speech = argostranslate.translate.translate(eng_speech, from_code, to_code)
-
+    if jp_speech:
         logger.info(f'Japanese: {jp_speech}')
-        logger.debug(f"translated | took {time.time() - start}")
-
+        logger.debug(f"transcript + translate | took {time.time() - start}")
         # speak(jp_speech, TARGET_LANGUAGE)
 
     else:
@@ -136,20 +113,19 @@ if __name__ == '__main__':
     # get channels and sampling rate of mic
     mic_info = p.get_device_info_by_index(MIC_ID)
     MIC_CHANNELS = mic_info['maxInputChannels']
+    print(f"MIC_CHANNELS: {MIC_CHANNELS}")
     MIC_SAMPLING_RATE = int(mic_info['defaultSampleRate'])
+    print(f"MIC_SAMPLING_RATE: {MIC_SAMPLING_RATE}")
 
     frames = []
     recording = False
-    stream = None
+    stream: Optional[pyaudio.Stream] = None
 
-    # Set DeepL or Google Translator
-    if USE_DEEPL:
-        translator = deepl.Translator(DEEPL_AUTH_KEY)
-    else:
-        translator = googletrans.Translator()
-
-    keyboard.on_press_key(RECORD_KEY, on_press_key)
-    keyboard.on_release_key(RECORD_KEY, on_release_key)
+    listener = keyboard.Listener(
+        on_press=on_press_key,
+        on_release=on_release_key,
+    )
+    listener.start()
 
     try:
         while True:
